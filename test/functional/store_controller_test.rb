745d1c3d283313afdbc8f4e14ae6ec293258a310
Lucie Rocnakova <lucierocnakova@gmail.com>
2016-04-17 20:36:51 +0200

    Zobrazení katalogu

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'MyString1'
+    assert_select '.price', /[\s\d]+\,\d\d Kč/
   end
 
 end
