0f08ef59fe0d5c14e67cb7ad4429d8453b2d4295
Lucie Rocnakova <lucierocnakova@gmail.com>
2016-04-19 14:31:05 +0200

    Chytřejší košík

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01, :message => 'Cena musí být větší než 0.'}
@@ -7,4 +9,15 @@ class Product < ActiveRecord::Base
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
 									:message => 'Musí jít o adresu URL pro obrázek typu GIF, JPG nebo PNG.'
 									}
+  private
+
+    # ujistí se, že se na tento produkt neodkazují žádné položky
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Existují položky')
+        return false
+      end
+    end
 end
