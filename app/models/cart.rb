1856ce11cc2d33d8e69297c0cb08e39f0353b2fb
Lucie Rocnakova <lucierocnakova@gmail.com>
2016-04-20 11:21:18 +0200

    Oprava celkového vyúčtování

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,8 +9,8 @@ def add_product(product_id)
     current_item = line_items.build(:product_id => product_id)
   end
   current_item
+end
 def total_price
   line_items.to_a.sum { |item| item.total_price }
-end
 end
 end
